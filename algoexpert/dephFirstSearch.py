832f2b226eee09437631110a32066e73d03f1c5c
ardakkk <ardakfuse@gmail.com>
2019-06-05 18:45:21 +0600

    algoexpert: Depth first search

---
+++ algoexpert/dephFirstSearch.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+    
+    def addChild(self, name):
+        self.children.append(Node(name))
+    
+    # O(v + e) time | O(v) space
+    def depthFirstSearch(self, array):
+        array.append(self.name)
+        for child in self.children:
+            child.depthFirstSearch(array)
+        return array
+
+result = Node('F').depthFirstSearch(['A', ['B', ['E'], ['F', ['I'], ['J']]], ['C'], ['D', ['G', ['F']], ['H']]])
+print(result)
