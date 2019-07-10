bdc986591906be18928f3239f60c44338abc10e9
ardakkkk <a.tileu@infit-it.kz>
2019-07-10 22:30:03 +0600

    acmp.ru | Euler phi function problem solved

---
+++ acmp.ru/acmp-1172-euler-phi.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int phi (int n) {
+	int result = n;
+	for (int i=2; i*i<=n; ++i)
+		if (n % i == 0) {
+			while (n % i == 0)
+				n /= i;
+			result -= result / i;
+		}
+	if (n > 1)
+		result -= result / n;
+	return result;
+}
+
+int main() {
+	int num;
+	cin >> num;
+    int result = phi(num);
+    cout << result << endl;
+    return 0;
+}
