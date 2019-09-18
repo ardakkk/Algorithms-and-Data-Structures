5c1727ef1605f3c397f1355dd856a786aa7f553e
ardakkk <ardakfuse@gmail.com>
2019-09-18 16:42:28 +0600

    leetcode | Solved maximum subarray problem

---
+++ leetcode/53-maximum-subarray.py
@@ -0,0 +1,10 @@
+# Time: O(n)
+from typing import List
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        for i in range(1, len(nums)):
+            nums[i] = max(nums[i], nums[i]+nums[i-1])
+        return max(nums)
+
+print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
