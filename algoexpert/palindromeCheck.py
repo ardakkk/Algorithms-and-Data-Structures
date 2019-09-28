2731ce6144f471b98c18a03bec4d97d733786f77
ardakkk <ardakfuse@gmail.com>
2019-09-28 22:57:04 +0600

    algoexpert | the best way to solve of palindrome checker

--- algoexpert/palindromeCheck.py
+++ algoexpert/palindromeCheck.py
@@ -15,8 +15,22 @@
 #     return string == "".join(reversedChars)
 
 # Time: O(n) | Space: O(n)
-def isPlaindrome(string, i = 0):
-    j = len(string) - 1 - i
-    return True if i >= j else string[i] == string[j] and isPlaindrome(string, i + 1)
+# def isPlaindrome(string, i = 0):
+#     j = len(string) - 1 - i
+#     return True if i >= j else string[i] == string[j] and isPlaindrome(string, i + 1)
 
-print(isPlaindrome("abcdcba"))
+# Time: O(n) | Space: O(n)
+def isPlaindrome(string):
+    leftIdx = 0
+    rightIdx = len(string) - 1
+
+    while leftIdx < rightIdx:
+        if string[leftIdx] != string[rightIdx]:
+            return False
+
+        leftIdx += 1
+        rightIdx -= 1
+
+    return True
+
+print(isPlaindrome('abcdcba'))
