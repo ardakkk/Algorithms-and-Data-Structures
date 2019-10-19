60567bea689bf55235ca540c6b0697bf0298606e
ardakkk <ardakfuse@gmail.com>
2019-10-19 11:08:51 +0600

    algoexpert | Implemented Linked list

--- algoexpert/linkedList.py
+++ algoexpert/linkedList.py
@@ -3,15 +3,71 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
 
+    # O(1) time | O(1) space
     def setHead(self, node):
-        pass
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            return
+        self.insertBefore(self.head, node)
 
+    # O(1) time | O(1) space
     def setTail(self, node):
-        pass
+        if self.tail is None:
+            self.setHead(node)
+            return
+        self.insertBefore(self.tail, node)
 
-    def removeNodesWithValue:
-        pass
+    # O(1) time | O(1) space
+    def insertAfter(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node
+        nodeToInsert.next = node.next
+        if node.next is None:
+            self.tail = nodeToInsert
+        else:
+            node.next.prev = nodeToInsert
+        node.next = nodeToInsert
 
+    # O(1) time | O(1) space
+    def insertBefore(self, node, nodeToInsert):
+        if nodeToInsert == self.head and nodeToInsert == self.tail:
+            return
+        self.remove(nodeToInsert)
+        nodeToInsert.prev = node.prev
+        nodeToInsert.next = node
+        if node.prev is None:
+            self.head = nodeToInsert
+        else:
+            node.prev.next = nodeToInsert
+        node.prev = nodeToInsert
+
+    # O(p) time | O(1) space
+    def insertAtPosition(self, position, nodeToInsert):
+        if position == 1:
+            self.setHead(nodeToInsert)
+            return
+        node = self.head
+        currentPosition = 1
+        while node is not None and currentPosition != position:
+            node = node.next
+            currentPosition += 1
+        if node is not None:
+            self.insertBefore(node, nodeToInsert)
+        else:
+            self.setTail(nodeToInsert)
+
+    def removeNodesWithValue(self, value):
+        node = self.head
+        while node is not None:
+            nodeToRemove = node
+            node = node.next
+            if node.value == value:
+                self.remove(nodeToRemove)
+
+    # O(1) time | O(1) space
     def remove(self, node):
         if (node == self.head):
             self.head = self.head.next
@@ -20,6 +76,7 @@ class DoublyLinkedList:
             self.tail = self.tail.prev
         self.removeNodeBindings(node)
 
+    # O(n) time | O(1) space
     def containsNodeWithValue(self, value):
         node = self.head
 
