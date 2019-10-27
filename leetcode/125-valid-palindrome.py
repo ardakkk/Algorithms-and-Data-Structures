463cfda9c5e32d6fa279f51d85ac25cf18ded795
ardakkk <ardakfuse@gmail.com>
2019-10-27 10:41:22 +0600

    leetcode | Second solution of Palindrome problem

--- leetcode/125-valid-palindrome.py
+++ leetcode/125-valid-palindrome.py
@@ -7,9 +7,27 @@
 # Output: False
 
 import string
+import re
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = [val.lower() for val in s if val not in string.punctuation and val not in string.whitespace]
         return s[:] == s[::-1]
 
+# Time: O(n) | Space: O(1) left and right Pointers take up constant space
+class Solution2:
+    def isPalindrome(self, s):
+        s = re.sub(r'[\W_]', '', s).lower()
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True
+
+print(Solution2().isPalindrome('A man, a plan, a canal: Panama'))
+print(Solution2().isPalindrome('race a car'))
