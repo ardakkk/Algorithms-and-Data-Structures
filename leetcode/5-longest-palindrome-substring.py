f565d2042114c0eb8b33d8b1d925e6bb77ad1b71
ardakkk <ardakfuse@gmail.com>
2019-10-27 10:55:59 +0600

    leetcode | Longest Palindrome substring problem is solved

---
+++ leetcode/5-longest-palindrome-substring.py
@@ -0,0 +1,25 @@
+# Given a string s, find the longest palindromic substring in s
+# --- Example
+# Input: "babad"
+# Output: "bab"
+# Note: "aba" is also a valid answer.
+
+# Time: (n^2) | Space: O(1)
+class Solution:
+    def longestPalindrome(self, s):
+        res = ""
+
+        for i in range(len(s)):
+            current = self.expand_around_middle(s, i - 1, i + 1)
+            in_between = self.expand_around_middle(s, i, i + 1)
+            res = max(res, current, in_between, key=len)
+
+        return res
+
+    def expand_around_middle(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left + 1: right]
+
+print(Solution().longestPalindrome('babad'))
