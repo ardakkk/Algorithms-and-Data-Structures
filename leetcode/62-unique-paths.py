7e93510bb7ab4356050074117d1e666664d082fa
ardakkk <ardakfuse@gmail.com>
2019-11-09 17:03:14 +0600

    leetcode | Unique paths problem is solved

---
+++ leetcode/62-unique-paths.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp_matrix = [[1 for col in range(m)] for row in range(n)]
+
+        for row in range(1, n):
+            for col in range(1, m):
+                dp_matrix[row][col] = dp_matrix[row][col - 1] + dp_matrix[row - 1][col]
+
+        return dp_matrix[-1][-1]
+    
