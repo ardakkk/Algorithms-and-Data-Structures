21cab7ebaeaf9f3d387e22c67f74467b01d302db
ardakkk <ardakfuse@gmail.com>
2019-11-09 17:08:33 +0600

    leetcode | Unique paths problem is solved

--- leetcode/62-unique-paths.py
+++ leetcode/62-unique-paths.py
@@ -1,3 +1,4 @@
+# Time: O(n + m) Where N and M are dimensions of Matrix | Space: (n + m) DP Matrix Size depends on inputs
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         dp_matrix = [[1 for col in range(m)] for row in range(n)]
@@ -7,4 +8,3 @@ class Solution:
                 dp_matrix[row][col] = dp_matrix[row][col - 1] + dp_matrix[row - 1][col]
 
         return dp_matrix[-1][-1]
-    
