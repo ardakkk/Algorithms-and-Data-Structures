d05d7b0500fa5eeed7178890e684151351a5000c
ardakkk <ardakfuse@gmail.com>
2019-11-17 22:31:55 +0600

    leetcode | Maximum product subarray problem is solved

--- leetcode/152-maximum-product-subarray.py
+++ leetcode/152-maximum-product-subarray.py
@@ -3,13 +3,16 @@ class Solution:
     def maxProduct(self, nums):
         max_till_index = [nums[0]]
         min_till_index = [nums[0]]
-        max_product = nums[0]
+        max_product    = nums[0]
 
         for i in range(1, len(nums)):
-             num = nums[i]
-             max_till_index.append(max(num, num * max_till_index[i - 1]), num * min_till_index[i - 1])
-             min_till_index.append(min(num, num * max_till_index[i - 1]), num * min_till_index[i - 1])
-             max_product = max(max_product, max_till_index[i])
+            num = nums[i]
+            max_till_index.append(max(num, num * max_till_index[i - 1], num * min_till_index[i - 1]))
+            min_till_index.append(min(num, num * max_till_index[i - 1], num * min_till_index[i - 1]))
+            max_product = max(max_product, max_till_index[i])
 
         return max_product
+        
 
+solution = Solution()
+print(solution.maxProduct([2, 3,-2 , 4]))
