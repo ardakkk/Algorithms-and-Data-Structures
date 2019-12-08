4608f13336aeb3345f61f088fddae6d9849c30bb
ardakkk <ardakfuse@gmail.com>
2019-12-08 12:00:11 +0600

    leetcode | Merge two sorted lists

--- leetcode/21-merge-two-sorted-lists.py
+++ leetcode/21-merge-two-sorted-lists.py
@@ -1,18 +1,44 @@
+# class Solution:
+#     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+#
+#         head = sort_list = ListNode(0)
+#
+#         while (l1 and l2):
+#             if (l1.val < l2.val):
+#                 sort_list.next = l1
+#                 l1 = l1.next
+#                 sort_list = sort_list.next
+#
+#             elif (l1.val >= l2.val):
+#                 sort_list.next = l2
+#                 l2 = l2.next
+#                 sort_list = sort_list.next
+#
+#         sort_list.next = l1 or l2
+#         return head.next
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+# Time: O(N + M) Where N and M are length of Linked Lists
+# Space: O(1) We always use three nodes, regardless of size of LL
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        dummy_head = ListNode("Placeholder")
+        tail = dummy_head
 
-        head = sort_list = ListNode(0)
-
-        while (l1 and l2):
-            if (l1.val < l2.val):
-                sort_list.next = l1
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
                 l1 = l1.next
-                sort_list = sort_list.next
-
-            elif (l1.val >= l2.val):
-                sort_list.next = l2
+            else:
+                tail.next = l2
                 l2 = l2.next
-                sort_list = sort_list.next
 
-        sort_list.next = l1 or l2
-        return head.next
+            tail = tail.next
+
+        tail.next = l1 or l2
+
+        return dummy_head.next
