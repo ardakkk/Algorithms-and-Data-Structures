456e3a3b789e839d52d21998070b8593ee3f3e46
ardakkk <ardakfuse@gmail.com>
2019-12-10 17:25:50 +0600

    leetcode | Verify preorder serialization of a binary tree

--- leetcode/331-verify-preorder-serialization-of-a-binary-tree.py
+++ leetcode/331-verify-preorder-serialization-of-a-binary-tree.py
@@ -1,17 +1,44 @@
 # Time: 0(n) We go over every character in Input String
 # Space: O(n) Splitting the string into an Array takes up memory
+# class Solution:
+#     def isValidSerialization(self, preorder: str) -> bool:
+#         p = preorder.split(',')
+#         slots = 1
+#
+#         for node in p:
+#             slots -= 1
+#
+#             if slots < 0:
+#                 return False
+#
+#             if node != "#":
+#                 slots += 2
+#
+#         return slots == 0
+
+# Time: O(n) We go over every character in Input String
+# Space: O(1)
 class Solution:
     def isValidSerialization(self, preorder: str) -> bool:
-        p = preorder.split(',')
         slots = 1
 
-        for node in p:
+        for i in range(len(preorder)):
+            char = preorder[i]
+
+            if char == ",":
+                slots -= 1
+
+                if slots < 0:
+                    return False
+
+                prev_char = preorder[i - 1]
+
+                if prev_char != "#":
+                    slots += 2
+
+        if preorder[-1] == "#":
             slots -= 1
-
-            if slots < 0:
-                return False
-
-            if node != "#":
-                slots += 2
+        else:
+            slots += 1
 
         return slots == 0
