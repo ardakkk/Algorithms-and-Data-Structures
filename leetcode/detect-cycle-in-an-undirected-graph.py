ab9d4869630defa60283c753b69c31b1299fd405
ardakkk <ardakfuse@gmail.com>
2019-12-17 21:53:13 +0600

    leetcode | Detect cycle in an undirected graph

---
+++ leetcode/detect-cycle-in-an-undirected-graph.py
@@ -0,0 +1,43 @@
+# Time: O(V + E) Same time complexity as DFS.
+# Space: O(V + E) We stored our Graph as an Adjacency List.
+class Solution:
+    def hasCycle(self, N, edges):
+        adjacency_list = {}
+        visited = {}
+
+        for vertex in range(N):
+            adjacency_list[vertex] = []
+            visited[vertex] = False
+
+        for edge in edges:
+            v1 = edge[0]
+            v2 = edge[1]
+
+            adjacency_list[v1].append(v2)
+            adjacency_list[v2].append(v1)
+
+        def dfs(vertex, parent):  # returns true if cycle detected and vice versa
+            visited[vertex] = True
+
+            for neighbor_vertex in adjacency_list[vertex]:
+                if not visited[neighbor_vertex]:
+                    if dfs(neighbor_vertex, vertex):
+                        return True
+                else:
+                    if neighbor_vertex != parent:
+                        return True
+
+            return False
+
+        for vertex in range(N):
+            if not visited[vertex]:
+                if dfs(vertex, -1):
+                    return True
+
+        return False
+
+
+
+
+
+
