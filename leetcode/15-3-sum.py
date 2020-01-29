541c7a38defeb993e2f0d98a89c42625ef6735c2
ardakkk <ardakfuse@gmail.com>
2020-01-29 16:56:57 +0600

    leetcode | Three Sum

--- leetcode/15-3-sum.py
+++ leetcode/15-3-sum.py
@@ -1,4 +1,4 @@
-# Time: O(n^2) | Space: O(n)
+# Time: O(n^2) | Space: O(1)
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
@@ -26,3 +26,31 @@ class Solution:
         return ans
 
 print(Solution().threeSum([-1, 0, 1, 2, -1, -4]))
+
+# Time: O(n^2)
+# Space: O(1)
+class Solution2:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            # two sum
+            j = i + 1
+            k = len(nums) - 1
+            while j < k:
+                if nums[i] + nums[j] + nums[k] == 0:
+                    res.append([nums[i], nums[j], nums[k]])
+                    while j < k and nums[j] == nums[j + 1]:
+                        j += 1
+                    while j < k and nums[k] == nums[k - 1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif nums[i] + nums[j] + nums[k] > 0:
+                    k -= 1
+                else:
+                    j += 1
+        return res
+        # end two sum
