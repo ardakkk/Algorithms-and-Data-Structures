2849ec16c445b7645d3044b90c03fb3397b4d191
ardakkk <ardakfuse@gmail.com>
2020-03-03 15:28:06 +0600

    leetcode | Unique paths

--- leetcode/62-unique-paths.py
+++ leetcode/62-unique-paths.py
@@ -8,3 +8,20 @@ class Solution:
                 dp_matrix[row][col] = dp_matrix[row][col - 1] + dp_matrix[row - 1][col]
 
         return dp_matrix[-1][-1]
+
+class Solution2:
+    def uniquePaths(self, m, n):
+        matrix = []
+
+        for i in range(m):
+            matrix.append([0] * n)
+
+        for i in range(m):
+            matrix[i][0] = 1
+
+        for j in range(n):
+            matrix[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                matrix[i][j] = matrix[i][j - 1] + matrix[i - 1][j]
+        return matrix[m - 1][n - 1]
