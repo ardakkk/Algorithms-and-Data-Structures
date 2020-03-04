254b4f483c00263dc244e7ee2c4256b6548512b0
ardakkk <ardakfuse@gmail.com>
2020-03-04 18:58:13 +0600

    leetcode | Remove zero sum consecutive node from linked list

---
+++ leetcode/1171-remove-zero-sum-consecutive-node-from-linked-list.py
@@ -0,0 +1,30 @@
+import collections
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def removeZeroSumSublists(self, head):
+        curr = dummy = ListNode(0)
+        dummy.next = head
+        prefix = 0
+        seen = collections.OrderedDict()
+
+        while curr:
+            prefix += curr.val
+
+            if prefix not in seen:
+                seen[prefix] = curr
+            else:
+                node = seen[prefix]
+                node.next = curr.next
+                while list(seen.keys())[-1] != prefix:
+                    seen.popitem()
+            curr = curr.next
+
+        return dummy.next
