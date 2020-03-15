91b8f46c56a94587690e7cdf664fc7d86cd95cd0
ardakkk <ardakfuse@gmail.com>
2020-03-15 23:53:51 +0600

    leetcode | Generate Parentheses

---
+++ leetcode/22-generate-parentheses.py
@@ -0,0 +1,15 @@
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def generateParenthesis(self, n):
+        res = []
+        def backtrack(str, left, right):
+            if len(str) == 2*n:
+                res.append(str)
+                return
+            if left < n:
+                backtrack(str + '(', left + 1, right)
+            if left >right:
+                 backtrack(str + ')', left, right + 1)
+        backtrack('', 0, 0)
+        return res
