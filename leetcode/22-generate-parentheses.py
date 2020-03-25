bae161991f029fc6d143bb396ad2f0c057e89503
ardakkk <ardakfuse@gmail.com>
2020-03-25 19:15:49 +0600

    leetcode | Generate Parentheses

--- leetcode/22-generate-parentheses.py
+++ leetcode/22-generate-parentheses.py
@@ -3,13 +3,15 @@
 class Solution:
     def generateParenthesis(self, n):
         res = []
+
         def backtrack(str, left, right):
-            if len(str) == 2*n:
+            if len(str) == 2 * n:
                 res.append(str)
                 return
             if left < n:
                 backtrack(str + '(', left + 1, right)
-            if left >right:
-                 backtrack(str + ')', left, right + 1)
+            if left > right:
+                backtrack(str + ')', left, right + 1)
+
         backtrack('', 0, 0)
         return res
