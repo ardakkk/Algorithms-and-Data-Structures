83a9fc64d7ee426779ef9980b56ea3d76bfdc1e0
ardakkk <ardakfuse@gmail.com>
2020-04-02 15:11:25 +0600

    leetcode | Meeting rooms II

--- leetcode/253-meeting-rooms-II.py
+++ leetcode/253-meeting-rooms-II.py
@@ -27,3 +27,35 @@ class Solution:
 solution = Solution()
 print(solution.minMeetingRooms([(0, 30), (5, 10), (15, 20)]))
 
+# Time: O(n log n)
+# Space: O(n)
+class Solution2:
+    def minMeetingRooms(self, intervals):
+        start = []
+        end = []
+
+        for i in intervals:
+            start.append(i[0])
+            end.append(i[1])
+
+        start.sort()
+        end.sort()
+
+        s = 0
+        e = 0
+
+        numRooms = 0
+        available = 0
+
+        while s < len(start):
+            if start[s] < end[e]:
+                if available:
+                    available -= 1
+                else:
+                    numRooms += 1
+                s += 1
+            else:
+                available += 1
+                e += 1
+
+        return numRooms
