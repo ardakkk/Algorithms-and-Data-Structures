b4881cb4d370dfb5c8234f4f46bf8c3473d561c7
ardakkk <ardakfuse@gmail.com>
2020-04-03 15:11:14 +0600

    leetcode | Number of Islands

--- leetcode/200-number-of-islands.py
+++ leetcode/200-number-of-islands.py
@@ -20,3 +20,30 @@ class Solution:
         self.sink(grid, row - 1, col)
         self.sink(grid, row, col + 1)
         self.sink(grid, row, col - 1)
+
+class Solution2:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def sinkIsland(grid, r, c):
+            if grid[r][c] == '1':
+                grid[r][c] = '0'
+            else:
+                return
+
+            if r + 1 < len(grid):
+                sinkIsland(grid, r + 1, c)
+            if r - 1 >= 0:
+                sinkIsland(grid, r - 1, c)
+            if c + 1 < len(grid[0]):
+                sinkIsland(grid, r, c + 1)
+            if c - 1 >= 0:
+                sinkIsland(grid, r, c - 1)
+
+        counter = 0
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    counter += 1
+                    sinkIsland(grid, i, j)
+
+        return counter
