ebe22f66fa98efef97f0d5f0658696e36e87abc0
ardakkk <ardakfuse@gmail.com>
2020-04-07 18:32:24 +0600

    leetcode | Maximum Depth of Binary Tree

--- leetcode/104-maximum-depth-of-binary-tree.py
+++ leetcode/104-maximum-depth-of-binary-tree.py
@@ -50,3 +50,29 @@ class Solution2:
 
         return max_depth
 
+# Time: O(n)
+# Space: O(n)
+class Solution2:
+    def maxDepth(self, root):
+        # base case
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.right), self.maxDepth(root.left)) + 1
+
+# Time: O(n)
+# Space: O(n)
+class Solution3:
+    def maxDepth(self, root):
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                stack.append((current_depth + 1, root.left))
+                stack.append((current_depth + 1, root.right))
+
+        return depth
