7a544041710c2225bc864d5b4745baa84bcde9e8
ardakkk <ardakfuse@gmail.com>
2020-04-08 23:00:56 +0600

    leetcode | Move Zeroes

---
+++ leetcode/283-move-zeroes.py
@@ -0,0 +1,16 @@
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+
+        for num in nums:
+            if (num != 0):
+                nums[j] = num
+                j += 1
+
+        for x in range(j, len(nums)):
+            nums[x] = 0
