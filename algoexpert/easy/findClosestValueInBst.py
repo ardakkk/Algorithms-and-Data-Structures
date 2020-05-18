1804e2af86d61c68efc6a9c8dca422ce0b1293ef
ardakkk <ardakfuse@gmail.com>
2020-05-18 06:08:02 +0600

    algoexpert | Find Closest Value In BST

--- algoexpert/easy/findClosestValueInBst.py
+++ algoexpert/easy/findClosestValueInBst.py
@@ -1,3 +1,9 @@
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
 # Average: O(log(n)) time | O(log(n)) space
 # Worst: O(n) time | O(n) space
 
@@ -34,3 +40,25 @@ def findClosestValueInBstHelper(tree, target, closest):
         else:
             break
     return closest
+
+#             9
+#           /  \
+#          4   17
+#         / \    \
+#        3   6    22
+#           / \   /
+#          5   7  20
+
+node = Node(9)
+node.left = Node(4)
+node.left.left = Node(3)
+node.left.right = Node(6)
+node.left.right.left = Node(5)
+node.left.right.right = Node(7)
+node.right = Node(17)
+node.right.right = Node(22)
+node.right.left = Node(20)
+
+print(findClosestValueInBst(node, 4))  # 4
+print(findClosestValueInBst(node, 18)) # 17
+print(findClosestValueInBst(node, 12)) # 9
