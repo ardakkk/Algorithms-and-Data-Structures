f36aa8ab3bfe36c400dfc224421c0fd5a4451980
ardakkk <ardakfuse@gmail.com>
2020-05-27 07:51:59 +0600

    algoexpert | productSum

---
+++ algoexpert/easy/productSum.py
@@ -0,0 +1,10 @@
+# Time: O(n) | Space: O(d) is depth
+def productSum(array, multiplier = 1):
+    sum = 0
+
+    for element in array:
+        if type(element) is list:
+            sum += productSum(element, multiplier + 1)
+        else:
+            sum += element
+    return sum * multiplier
