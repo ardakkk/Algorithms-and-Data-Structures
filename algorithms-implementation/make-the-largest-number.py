2b409a547c20f0574f95b43ee50b59af3af933c3
ardakkk <ardakfuse@gmail.com>
2020-06-12 15:06:50 +0600

    algorithms implementation | Make the Largest Number

---
+++ algorithms-implementation/make-the-largest-number.py
@@ -0,0 +1,19 @@
+"""
+Make the Largest Number
+Given a number of integers, combine them so it would create the largest number.
+
+Example:
+Input: [17, 7, 2, 45, 72]
+Output: 77245217
+"""
+from functools import cmp_to_key
+def largestNum(nums):
+    def compare(a, b):
+        if str(a) + str(b) < str(b) + str(a):
+            return 1
+        else:
+            return -1
+
+    str_nums = [str(n) for n in sorted(nums, key=cmp_to_key(compare))]
+    return "".join(str_nums)
+print(largestNum([17, 7, 2, 45, 72]))
